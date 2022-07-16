6c88ea8a7c21f121709a80578a066b1cee448c90
Ezra <ezra.sunshine@gmail.com>
2022-07-16 12:10:28 -0400

    init

--- src/index.jsx
+++ src/index.jsx
@@ -1,11 +1,11 @@
 import * as React from "react";
-import ReactDOM from "react-dom";
+import { createRoot } from 'react-dom/client';
 import "./index.css";
-import App from "./App";
+import London from "./Map";
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById("root")
-);
+const container = document.getElementById("root");
+const root = createRoot(container);
+
+// root.render(<App />);
+root.render(<London />);
+
